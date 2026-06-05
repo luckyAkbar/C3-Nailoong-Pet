9dfdc05b1b9b02c1497e351f0fcf29f7584b378e
Lucky Akbar <55319599+luckyAkbar@users.noreply.github.com>
2026-06-06 00:14:12 +0700

    Revert "Feature/design tokens fakhri"

--- NailongPet/Core/DesignTokens/Color.swift
+++ NailongPet/Core/DesignTokens/Color.swift
@@ -2,32 +2,17 @@
 //  Color.swift
 //  NailongPet
 //
-//  Design Token - Single Source of Truth untuk semua warna aplikasi
+//  Created by Lucky Akbar on 05/06/26.
 //
 
 import SwiftUI
 
 extension Color {
-    // MARK: - Brand Colors
-    static let orangePrimaryBrand = Color(hex: 0xC9865A)
-    static let brownSecondaryBrand = Color(hex: 0x8D7966)
-    static let beigeTertiaryBrand = Color(hex: 0xE2DEDB)
-    
-    // MARK: - Text Colors
-    static let blackPrimaryText = Color(hex: 0x000000)
-    static let graySecondaryText = Color(hex: 0x8E8E93)
-    
-    // MARK: - Surface Colors
-    static let whitePrimarySurface = Color(hex: 0xFFFFFF)
-    static let blackSecondarySurface = Color(hex: 0x000000)
-    
-    // MARK: - Action & Utility Colors
-    static let blueDefaultAction = Color(hex: 0x007AFF)   // Label fully rounded / tips
-    static let greenSucceedAction = Color(hex: 0x34C759)   // Button ARAction Succeed
-    static let grayDisabledAction = Color(hex: 0x8E8E93)   // Button ARAction Disabled
+    static let BrandColorPrimary = Color(hex: 0xC9865A)
+    static let BrandColorSecondary = Color(hex: 0x8D7966)
+    static let BrandColorTertiary = Color(hex: 0xE2DEDB)
 }
 
-// MARK: - Hex Initializer
 extension Color {
     init(hex: UInt, alpha: Double = 1.0) {
         self.init(
--- NailongPet/Core/DesignTokens/Icon.swift
+++ NailongPet/Core/DesignTokens/Icon.swift
@@ -2,30 +2,9 @@
 //  Icon.swift
 //  NailongPet
 //
-//  Design Token - Single Source of Truth untuk semua ikon SF Symbol aplikasi
+//  Created by Lucky Akbar on 05/06/26.
 //
 
-import Foundation
-import SwiftUI
-
-enum AppIcon: String {
-    // MARK: - Button Actions
-    case add = "plus"
-    case edit = "pencil"
-    case close = "xmark"
-    case check = "checkmark"
-    case back = "chevron.left"
-    case infoTips = "lightbulb"
-    case moli = "Moli"
-    
-    // MARK: - App Features & States
-    case bringTo3D = "square.stack"
-    case pawLoading = "pawprint.fill"
-    case cameraGuideline = "camera"
-    case recordingPause = "pause.circle"
-    
-    /// Properti pembantu untuk langsung mengeluarkan komponen Image siap pakai
-    var image: Image {
-        return Image(systemName: self.rawValue)
-    }
+enum AppIcon {
+    static let MenuFeatureDefault = "square.2.layers.3d"
 }

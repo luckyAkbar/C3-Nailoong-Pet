02aa0ca7205aacb9c70eff983cc10f81e495411d
luckyAkbar <luckyakbar1509@gmail.com>
2026-06-05 15:30:20 +0700

    feat: initialize menu selection card component

--- NailongPet/Components/Card/MenuSelection/MenuSelection.swift
+++ NailongPet/Components/Card/MenuSelection/MenuSelection.swift
@@ -8,18 +8,24 @@
 import SwiftUI
 
 struct MenuSelection: View {
+    var ratio: CGFloat = 0.25 // Configurable ratio for the first part (e.g. 0.25 for 1/4 width)
+
     var body: some View {
         VStack{
             Image(systemName: "placeholderMenuSelection")
-                .frame(maxWidth: .infinity, maxHeight: 300)
+                .frame(maxWidth: 313, maxHeight:277)
                 .background(RoundedRectangle(cornerRadius: 22).fill(Color.white))
-            HStack{
-                Image(systemName: AppIcon.MenuFeatureDefault)
-                VStack{
-                    Text("Titlke").font(Font.title1)
-                    Text("Subtitles")
-                }.frame(maxWidth: .infinity)
-            }
+            
+                HStack {
+                    Image(systemName: AppIcon.MenuFeatureDefault)
+                        .font(Font.title1)
+                        .padding(10)
+                    VStack(alignment: .leading){
+                        Text("Bring Pet to 3D").font(Font.subhead)
+                        Text("Preserve the moment with your pet")
+                    }
+                    .frame(maxWidth: .infinity)
+                }
         }
         .padding(12)
         .background(RoundedRectangle(cornerRadius: 22).fill(Color.BrandColorPrimary))
